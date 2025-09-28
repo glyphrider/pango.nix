6157633c7492d55fc623f14d3bcb9da4a40e33b2
Brian H. Ward <glyphrider@gmail.com>
2025-09-28 08:47:49 -0400

    working home.nix

--- flake.nix
+++ flake.nix
@@ -3,15 +3,42 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    hyprland.url = "github:hyprwm/Hyprland";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    hyprland = {
+      url = "github:hyprwm/Hyprland";
+    };
+    hyprland-plugins = {
+      url = "github:hyprwm/hyprland-plugins";
+      inputs.hyprland.follows = "hyprland";
+    };
+
   };
 
-  outputs = { nixpkgs, ... } @ inputs: {
+  outputs = { self, nixpkgs, home-manager, hyprland, hyprland-plugins, ... } @ inputs:
 
-    nixosConfigurations.pango = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = { inherit inputs; };
-      modules = [ ./configuration.nix ];
+  let
+    system = "x86_64-linux";
+    pkgs = import inputs.nixpkgs { system = "${system}"; config.allowUnfree = true; };
+  
+  in {
+
+    nixosConfigurations = {
+      pango = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [ ./configuration.nix ];
+      };
+    };
+    homeConfigurations = {
+      "brian" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        extraSpecialArgs = {
+          inherit inputs;
+        };
+        modules = [ ./home.nix ];
+      };
     };
   };
 }
