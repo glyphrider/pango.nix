122eb7df6078ad42e2c4336b64e88e8e8b28deec
Brian H. Ward <glyphrider@gmail.com>
2025-09-29 10:42:46 -0400

    better editor

--- home.nix
+++ home.nix
@@ -101,7 +101,7 @@
   #  /etc/profiles/per-user/brian/etc/profile.d/hm-session-vars.sh
   #
   home.sessionVariables = {
-    # EDITOR = "emacs";
+    EDITOR = "nvim";
   };
 
   # Let Home Manager install and manage itself.
