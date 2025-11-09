67bbafaf1b9b2577a68287f0991c226b7270d876
Brian H. Ward <glyphrider@gmail.com>
2025-11-09 11:11:49 -0500

    aliases for hyprpaper

--- files/config.fish
+++ files/config.fish
@@ -2,6 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     alias r=ranger
     alias vim=nvim
+    alias anime='hyprctl hyprpaper wallpaper ",~/.local/share/wallpaper/wall2.png"'
+    alias logo='hyprctl hyprpaper wallpaper ",~/.local/share/wallpaper/wall0.png"'
 
     set -x EDITOR nvim
 end
